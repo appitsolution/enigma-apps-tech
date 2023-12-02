e9432ade12b4990abd8965170e014baf4f7ef0be
appitsolution <appitsolution@gmail.com>
2023-12-02 20:28:13 +0200

    added routing

---
+++ app/about/page.js
@@ -0,0 +1,3 @@
+export default function About() {
+  return <p className="test">Templates</p>;
+}
---
+++ components/Header.jsx
@@ -0,0 +1,36 @@
+import Image from "next/image";
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <div className="container">
+        <div className="header-block">
+          <a className="header-logo">
+            <Image className="header-logo-icon" />
+          </a>
+
+          <ul className="header-menu">
+            <li className="header-menu-item">
+              <a className="header-menu-link">About</a>
+            </li>
+            <li className="header-menu-item">
+              <a className="header-menu-link">Service</a>
+            </li>
+            <li className="header-menu-item">
+              <a className="header-menu-link">Portfolio</a>
+            </li>
+            <li className="header-menu-item">
+              <a className="header-menu-link">Price</a>
+            </li>
+            <li className="header-menu-item">
+              <a className="header-menu-link">Contacts</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
